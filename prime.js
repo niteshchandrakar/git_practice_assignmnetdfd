ba62efff0d76d906fce6576c4e9b96fde5bcdc32
Nitesh <niteshchandrakar2@gmail.com>
2022-10-20 19:30:06 +0530

    prime

---
+++ prime.js
@@ -0,0 +1,31 @@
+// for check prime
+let num = 13
+let count = 0
+    for(let i = 1 ; i<=num ; i--){
+        if(num%i==0){
+            count++
+            
+        }
+        
+    }
+    if(count==2){
+        console.log("Yes")
+    }
+    else{
+        console.log("No")
+    }
+
+for check palindrome
+let n = 6
+let str = madam
+let bag = ""
+for(let i = N-1 ; i>=0 ; i--){
+    bag+= str[i]
+}
+if(bag==str){
+    console.log("Yes")
+}
+else{
+    console.log("No")
+}
+  }
